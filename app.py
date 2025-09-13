262c0ce5de0fac0d0f3489c3c2950675431a88db
Thomas McInnes <tmcinnes@mines.edu>
2025-09-13 00:55:56 -0600

    changed implementation to use steam's web api, also going to pivot to tkinter

--- app.py
+++ app.py
@@ -1,109 +1,50 @@
-import random
-import sys
-import pygame
-import xml.etree.ElementTree as ET
+import requests
+import json
+from os import listdir
 
-pygame.init()
-pygame.display.set_caption("Steam Game Picker")
+def getKey(keyFile):
+    with open(keyFile, "r") as file:
+        key = file.readline()
+    return key
 
-WINDOW_WIDTH = 500
-WINDOW_HEIGHT = 500
+def saveInfo(user, data):
+    with open("./steam-picker/users/" + user + ".json", "w") as file:
+        json.dump(data, file)
 
-DISP = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+def loadInfoFromFile(user):
+    with open("./steam-picker/users/" + user + ".json", "r") as file:
+        data = json.load(file)
+    return data
 
-FPS = 60
+if __name__ == '__main__':
+    usersOnFile = listdir("./steam-picker/users/") # get the users in the users dir
+    # print(usersOnFile[0][:-5])
+    key = getKey("./steam-picker/steamweb.key") # get API key from file
+    print("enter steamid64 (decimal): ")
+    steamID = input()
 
-# game colors
-COLOR_BG = (255, 255, 255)
-COLOR_BUTTON = (0, 100, 0)
-COLOR_BLACK = (0, 0, 0)
+    loadFromFile = "\0"
+    if (steamID + ".json" in usersOnFile): # check if user id is already saved to a file to avoid api call
+        loadFromFile = "\0"
+        while (loadFromFile.lower() != "y" and loadFromFile.lower() != "n"):
+            print("user info is currently saved to a local file. load it? (y/n)\n" \
+            "(this is opposed to calling the steamweb api)")
+            loadFromFile = input()
 
-# fonts
-FONT_MAIN = pygame.font.SysFont("arial", 32)
+    jsonData = {}
+    if (loadFromFile.lower() == "y"):
+        jsonData = loadInfoFromFile(steamID)
+    else:
+        saveToFile = "\0"
+        while (saveToFile.lower() != "y" and saveToFile.lower() != "n"):
+            print("save owned game information to local file? (y/n)")
+            saveToFile = input()
 
-class Button:
-    def __init__(self, buttonText, x, y):
-        self.text = buttonText
-        self.x = x
-        self.y = y
-        self.width = 20
-        self.height = 50
-        self.leftBound = self.x - (len(self.text) * (self.width // 2))
-        self.rightBound = self.x + (len(self.text) * self.width)
-        self.textSurface = FONT_MAIN.render(buttonText, False, COLOR_BLACK)
-    def draw(self):
-        pygame.draw.rect(DISP, COLOR_BUTTON, (self.leftBound, self.y, (len(self.text) * self.width), self.height), width=0)
-        DISP.blit(self.textSurface, (self.leftBound, self.y + 5))
-    def checkBounds(self,mousePos): # mousePos is a tuple [int,int]
-        # print(mousePos[0], mousePos[1])
-        # print((self.x - (len(self.text) * (self.width // 2))), (len(self.text) * self.width))
-        if(mousePos[0] > self.leftBound and mousePos[0] < self.rightBound and mousePos[1] > self.y and mousePos[1] < (self.y + self.height)):
-            return True
-        else:
-            return False
-
-class TextDisplay:
-    def __init__(self, text, x, y):
-        self.text = text
-        self.x = x
-        self.y = y
-        self.textSurface = FONT_MAIN.render(text, False, COLOR_BLACK)
-    def draw(self):
-        DISP.blit(self.textSurface, (self.x, self.y))
-    def setText(self, newText):
-        self.textSurface = FONT_MAIN.render(newText, False, COLOR_BLACK)
-
-def parseXML(file):
-    gameList = []
-
-    tree = ET.parse(file)
-    root = tree.getroot()
-    for game in root.findall('./games/game/name'):
-        gameList.append(game.text)
-
-    return gameList
-
-def selectRandomItem(list):
-    return list[random.randint(0,len(list) - 1)]
-
-randomizeGameButton = Button("New Game",250,300)
-gameDisplay = TextDisplay("", 125, 150)
-
-if __name__ == "__main__":
-    # setup xml file into list
-    fullGameList = parseXML('steam-picker/tomgames.xml') + parseXML('steam-picker/savgames.xml')
-    fullGameList = set(fullGameList)
-    fullGameList = list(fullGameList)
-
-    tickCount = 0
-
-    rolling = False
-    counter = 0
-
-    # gameloop
-    while(True):
-        DISP.fill(COLOR_BG)
-
-        for event in pygame.event.get():
-            if event.type == pygame.MOUSEBUTTONUP:
-                if (randomizeGameButton.checkBounds(pygame.mouse.get_pos()) and not rolling):
-                    rolling = True
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-
-        if rolling:
-            if counter < tickCount:
-                gameDisplay.setText(selectRandomItem(fullGameList))
-                counter += 1
-                print(counter)
-            else:
-                rolling = False
-                counter = 0
-
-        randomizeGameButton.draw()
-        gameDisplay.draw()
-        pygame.display.update()
-
-        pygame.time.Clock().tick(FPS)
-        tickCount = (tickCount + 1) % 60
+        url = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=" + key + "&steamid=" + steamID + "&include_appinfo=1&include_played_free_games=1&format=json"
+        jsonData = requests.get(url).json()
+        
+        if (saveToFile.lower() == "y"):
+            saveInfo(steamID, jsonData)
+    
+    for game in jsonData["response"]["games"]:
+        print(game["name"])
